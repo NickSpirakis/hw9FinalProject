45d813b8e1b6858bf32cddf5f0f30c3b8c6f58b4
nespi <nespi@DESKTOP-6AI8FHS>
2019-12-06 20:18:44 -0600

    trying to commit

--- Final/src/UserInterface.java
+++ Final/src/UserInterface.java
@@ -11,7 +11,6 @@ public class UserInterface extends JFrame {
 	
 	public void setUpMenu() {
 		
-		
 		JMenuBar mbar = new JMenuBar();
 		JMenu menuFile = new JMenu("File"); //exit
 		JMenu menuHelp = new JMenu("Help"); //about
