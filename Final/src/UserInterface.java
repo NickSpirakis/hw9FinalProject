fcc912131b0f0341772efd50f759b1893702da3f
nespi <nespi@DESKTOP-6AI8FHS>
2019-12-06 20:12:55 -0600

    added and finished file and help menus

--- Final/src/UserInterface.java
+++ Final/src/UserInterface.java
@@ -9,11 +9,40 @@ public class UserInterface extends JFrame {
 	private String textToShow;
 	private JTextArea tx;
 	
+	public void setUpMenu() {
+		
+		JMenuBar mbar = new JMenuBar();
+		JMenu menuFile = new JMenu("File"); //exit
+		JMenu menuHelp = new JMenu("Help"); //about
+		
+		JMenuItem mbarExit = new JMenuItem("Exit");
+		mbarExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		JMenuItem mbarAbout = new JMenuItem("About");
+		mbarAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Made by Nick Spirakis and Adrian Perez");
+			}
+		});
+		menuFile.add(mbarExit);
+		mbar.add(menuFile);
+		
+		menuHelp.add(mbarAbout);
+		mbar.add(menuHelp);
+		
+		setJMenuBar(mbar);
+	}
 	public void setupUI() {
 		textToShow = "";
 		setTitle("Top Websites Visted Monthly");
 		setBounds(500, 500, 800, 800);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 		
@@ -51,14 +80,16 @@ public class UserInterface extends JFrame {
 		btnSaveToText.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//save to text
+				
 			}
 		});
 		
 		JButton btnSaveToJson = new JButton("Save to json");
-		btnSaveToJson.setBounds(150, 150, 300, 300);	//this should change size but on this one i dont think it does
+		btnSaveToJson.setBounds(150, 150, 300, 300);	//this should change size but on this one i don't think it does
 		btnSaveToText.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//save to json
+				
 			}
 		});
 		panSouth.add(btnSaveToText);
@@ -66,6 +97,7 @@ public class UserInterface extends JFrame {
 		
 		c.add(panSouth, BorderLayout.SOUTH);
 		
+		setUpMenu();
 		
 	}
 	
